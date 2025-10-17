20602ecb99eae4b3e6cc7ff7a5b8693c5550d90e
saobangmath <anhtai2672000@gmail.com>
2025-10-17 23:46:09 +0700

    TxnHandler.java

--- CryptoCurrencies/scroogeCoin/TxHandler.java
+++ CryptoCurrencies/scroogeCoin/TxHandler.java
@@ -1,5 +1,10 @@
-public class TxHandler {
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
+public class TxHandler {
+    private UTXOPool utxoPool;
     /**
      * Creates a public ledger whose current UTXOPool (collection of unspent transaction outputs) is
      * {@code utxoPool}. This should make a copy of utxoPool by using the UTXOPool(UTXOPool uPool)
@@ -7,6 +12,7 @@ public class TxHandler {
      */
     public TxHandler(UTXOPool utxoPool) {
         // IMPLEMENT THIS
+        this.utxoPool = new UTXOPool(utxoPool);
     }
 
     /**
@@ -20,6 +26,47 @@ public class TxHandler {
      */
     public boolean isValidTx(Transaction tx) {
         // IMPLEMENT THIS
+        Set<UTXO> usedUTXOs = new HashSet<>(); // make sure that there are no duplicate UTXOs
+        
+        double inputValueSum = 0, outputValueSum = 0;
+
+        for (int inputIndex = 0; inputIndex < tx.numInputs(); inputIndex++){
+            Transaction.Input input = tx.getInput(inputIndex);
+            UTXO utxo = new UTXO(input.prevTxHash, input.outputIndex);
+            
+            // condition 1, 3
+            if (!this.utxoPool.contains(utxo) || usedUTXOs.contains(utxo)) {
+                return false;
+            }
+
+            Transaction.Output output = this.utxoPool.getTxOutput(utxo); 
+            // condition 2
+            if (!Crypto.verifySignature(
+                output.address, 
+                tx.getRawDataToSign(inputIndex), 
+                input.signature)
+            ) {
+                return false;
+            }
+
+            inputValueSum += output.value;
+            usedUTXOs.add(utxo); 
+        }
+
+        for (Transaction.Output output : tx.getOutputs()){
+            // condition 4
+            if (output.value < 0) {
+                return false;
+            }
+
+            outputValueSum += output.value;
+        }
+
+        // condition 5
+        if (inputValueSum < outputValueSum) {
+            return false;
+        }
+        return true; 
     }
 
     /**
@@ -29,6 +76,26 @@ public class TxHandler {
      */
     public Transaction[] handleTxs(Transaction[] possibleTxs) {
         // IMPLEMENT THIS
+        List<Transaction> validTxs = new ArrayList<>();
+        for (Transaction tx : possibleTxs){
+            if (isValidTx(tx)) {
+                validTxs.add(tx);
+                this.addValidTx(tx);
+            }
+        }
+        return validTxs.toArray(new Transaction[validTxs.size()]);
     }
 
+    private void addValidTx(Transaction tx) {
+        for (int inputIndex = 0; inputIndex < tx.numInputs(); inputIndex++){
+            Transaction.Input input = tx.getInput(inputIndex);
+            UTXO utxo = new UTXO(input.prevTxHash, input.outputIndex);
+            this.utxoPool.removeUTXO(utxo);
+        }
+
+        for (int outputIndex = 0; outputIndex < tx.numOutputs(); outputIndex++){
+            Transaction.Output output = tx.getOutput(outputIndex); 
+            this.utxoPool.addUTXO(new UTXO(tx.getHash(), outputIndex), output);
+        }
+    }
 }
